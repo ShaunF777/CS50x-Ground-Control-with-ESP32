3ba88334814aa70b23aa962d804aae80128c1a9f
ShaunF777 <shaunfr777@gmail.com>
2024-12-09 16:02:09 +0200

    Main and remote ESP32 systems for ground moisture control

---
+++ remote-sense-esp32c3/boot.py
@@ -0,0 +1,22 @@
+import network
+import time
+
+# Add a delay to prevent bootlooping
+time.sleep(2)  # 2-second delay
+print("Starting main program...")
+
+def do_connect():
+ # Create instances for connection objects
+    sta_if = network.WLAN(network.STA_IF) # For ESP station to connect to a router 
+    ap_if = network.WLAN(network.AP_IF) # For others to connects to the ESP Access Point
+    ap_if.active(False) # Deactivate accespoint interface
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect('VODAFONEF77C_EXTender', '32HRGUM7K9FFGXLK')
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ipconfig('addr4'))
+
+do_connect()
+   
